c005e5da7341d9f83f97a33fb99a23a292d91aa5
jordan 208 <jordan@hoomiz.com>
2023-05-10 00:55:35 +0200

    fix mail

--- sendMail.ts
+++ sendMail.ts
@@ -1,8 +1,33 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import nodemailer from 'nodemailer';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
-  })
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  // Récupération des informations du corps de la requête HTTP
+  const { to, subject, message } = req.body;
+
+  // Configuration du transporteur de messagerie
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'votre_adresse_email@gmail.com',
+      pass: 'votre_mot_de_passe'
+    }
+  });
+
+  // Création de l'objet de messagerie
+  const mailOptions = {
+    from: 'votre_adresse_email@gmail.com',
+    to,
+    subject,
+    text: message
+  };
+
+  try {
+    // Envoi du message
+    await transporter.sendMail(mailOptions);
+    return res.status(200).json({ message: 'Message envoyé avec succès' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Erreur lors de l\'envoi du message' });
+  }
 }
